c4d5f4eb59830097b7447989935a819fd0cf6378
Diego <dantonio808@gmail.com>
2016-10-06 21:21:46 -0300

    Conexao e consultas com o banco funcionais

--- src/main/java/com/github/dantonio808/fileChest/app/RestAPIApplication.java
+++ src/main/java/com/github/dantonio808/fileChest/app/RestAPIApplication.java
@@ -1,12 +1,19 @@
 package com.github.dantonio808.fileChest.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.github.dantonio808.fileChest.api.interfaces.UserService;
+import com.github.dantonio808.fileChest.api.model.User;
+
 @EnableAutoConfiguration
 @ComponentScan("com.github.dantonio808.fileChest.api")
-public class RestAPIApplication {
+public class RestAPIApplication implements CommandLineRunner{
+	@Autowired
+	private UserService service;
 	
 	/**
 	 * Executa a aplicacao
@@ -16,4 +23,30 @@ public class RestAPIApplication {
 		SpringApplication.run(RestAPIApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg0) throws Exception {
+		User usuario = new User();
+		
+		usuario.setLogin("teste1");
+		usuario.setPasswd("teste1");
+		
+		this.service.addUser(usuario);
+		usuario = new User();
+		usuario.setLogin("teste4");
+		usuario.setPasswd("teste4");
+		
+		this.service.addUser(usuario);
+		usuario = new User();
+		usuario.setLogin("teste5");
+		usuario.setPasswd("teste5");
+		
+		this.service.addUser(usuario);
+		
+		System.out.println("Lista de Usuarios!");
+		this.service.findAll().forEach(user ->{
+			System.out.println("Usuario:");
+			System.err.println("Nome: " + user.getLogin());
+			System.err.println("Nome: " + user.getPasswd());
+		});
+	}
 }
