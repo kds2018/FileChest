c05534a8d6f5d76fad4cf43533a9c994b23e66d0
Diego <dantonio808@gmail.com>
2016-10-13 13:33:29 -0300

    estrutura dos testes

--- src/main/java/com/github/dantonio808/fileChest/api/database/DatabaseConfig.java
+++ src/main/java/com/github/dantonio808/fileChest/api/database/DatabaseConfig.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,7 +17,6 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @EnableJpaRepositories("com.github.dantonio808.fileChest.api.interfaces")
-@PropertySource("classpath:/application.properties")
 public class DatabaseConfig {
 	@Autowired
 	private Environment env;
